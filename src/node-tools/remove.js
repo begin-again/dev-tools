9d82212430d28086a539a4420bd0b6d1f99604af
Todd Hogarth <begin-again@users.noreply.github.com>
2023-09-01 12:58:56 +0000

    use async in NT remove (#81)

--- src/node-tools/remove.js
+++ src/node-tools/remove.js
@@ -4,43 +4,45 @@
  * Removes node versions
 */
 
-const { rmSync } = require('fs');
-const del = require('del');
+const { rmdir } = require('fs').promises;
 const semver = require('semver');
 
 /**
  * Removes node versions matching specified range
  *
  * @param {Object} param0
- * @param {Array<Version>} param0.installed
- * @param {String} param0.version - version number or range
- * @param {Boolean} param0.execute - true to remove files
- * @param {Object} [log] logger
- * @returns {Number} exit code
+ * @param {array<Version>} param0.installed
+ * @param {string} param0.version - version number or range
+ * @param {boolean} param0.execute - true to remove files
+ * @param {object} [log] logger
+ * @returns {Promise<number>} exit code
  */
 const remove = async ({ installed, version, execute }, log = console) => {
     const messagePrefix = execute ? 'Removed' : 'Would remove';
     const validRange = semver.validRange(version);
-    let matchesFound = 0;
     let exitCode = 0;
-    installed.forEach(v => {
-        if(semver.satisfies(v.version.slice(1), validRange)) {
-            matchesFound++;
-            if(execute) {
-                if(rmSync) {
-                    rmSync(v.path, { recursive: true });
-                }
-                else {
-                    del.sync(v.path, { force: true });
-                }
-            }
-            log.debug(`${messagePrefix} ${v.version} at ${v.path}`);
+    const versionsToRemove = installed.filter(v => semver.satisfies(v.version.slice(1), validRange));
+
+    if(versionsToRemove.length > 0) {
+        if(execute) {
+            await Promise.all(
+                versionsToRemove.map(async v => {
+                    await rmdir(v.path, { recursive: true });
+                    log.debug(`${messagePrefix} ${v.version} at ${v.path}`);
+                })
+            );
         }
-    });
-    if(matchesFound === 0) {
+        else {
+            versionsToRemove.forEach(v => {
+                log.debug(`${messagePrefix} ${v.version} at ${v.path}`);
+            });
+        }
+    }
+    else {
         log.debug(`No matches found for ${version} in range ${validRange}`);
         exitCode = 1;
     }
+
     return exitCode;
 };
 
